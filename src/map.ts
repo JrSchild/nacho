55113790c321dbb29cfd2ccd48a65489e965089a
Joram Ruitenschild <>
2019-12-17 13:32:58 +0100

    feat(map): inline the element iteration

--- src/map.ts
+++ src/map.ts
@@ -1,23 +1,13 @@
 import { Task, TaskIterable } from './common/types';
 
-/**
- * Returns an iterator that consumes the previousIterator and process each
- * element asynchronously with the handle. One by one.
- */
-const makeAsyncIterable = <InContext, OutContext>(
+export const map = <InContext, OutContext>(
   handle: (context: InContext) => Promise<OutContext> | OutContext,
-): ((
+): Task<InContext, OutContext> => (
   previousIterator: TaskIterable<InContext>,
-) => TaskIterable<OutContext>) => (
-  previousIterator: TaskIterable<InContext>,
-): TaskIterable<OutContext> => ({
+) => ({
   async *[Symbol.asyncIterator]() {
     for await (const taskResult of previousIterator) {
       yield await handle(taskResult);
     }
   },
 });
-
-export const map = <InContext, OutContext>(
-  handle: (context: InContext) => Promise<OutContext> | OutContext,
-): Task<InContext, OutContext> => makeAsyncIterable(handle);
